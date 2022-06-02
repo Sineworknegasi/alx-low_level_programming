b07f6ea21da5557f4b97a06146f17219a83c7eed
SINEWORK <Sineworknegasi38@gmail.com>
2022-06-02 08:46:48 -0700

    8. UNIX is basically a simple operating system,

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+/**
+ * main - prints the standard error sandwiched around the standard outpu/t
+ *
+ * Return: return 1
+ */
+int main(void)
+{
+write(2,"and that piece of art is useful\" - dora korpar, 2015-10-19\n",59);
+return (1);
+}
