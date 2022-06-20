2d2421521df9880a079df82f403ddd61ce126530
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2022-06-20 17:52:01 +0300

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,21 @@
-#include "main.h"
 /**
- *_memset -   fills memory with a constant byte
- *@s: pointer block of memory to fill
- *@b: value to set
- *@n: bytes of the memory
- *Return: dest
+ * _memset - fills n bytes of memory
+ *
+ * @s: pointer to start of memory area
+ * @b: constant byte to fill with
+ * @n: num bytes to fill in
+ *
+ * Return: pointer to beginning of memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
-	unsigned int i;
+	unsigned int i = 0;
+	char *start = s;
 
-	/*Delacring FOR*/
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		*(s + i) = b; /*add 1 position s*/
-
-	} /*END FOR*/
-
-	return (s);
+		*s++ = b;
+		i++;
+	}
+	return (start);
 }
