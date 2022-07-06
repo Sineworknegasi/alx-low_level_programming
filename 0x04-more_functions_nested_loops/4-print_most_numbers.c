c001ed367f1b52df881729ee69f7e2ee54cfc680
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2022-07-06 11:35:55 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 /**
  * print_most_numbers - print numbers
  *
@@ -6,18 +6,15 @@
  */
 void print_most_numbers(void)
 {
+        int i;
 
-int i;
-
-i = 0;
-while (i < 10)
-{
-if (i != 2 && i != 4)
-{
-_putchar (i + '0');
-}
-i++;
-}
-
-_putchar ('\n');
+        for (i = 0; i <= 9; i++)
+        {
+                if (i == 2 || i == 4)
+                {
+                        i++;
+                }
+                _putchar (i + '0');
+        }
+        _putchar ('\n');
 }
