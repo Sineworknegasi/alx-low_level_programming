1ff1d5c0152efdaad9db0cc8de17d98b9667a6b3
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2022-07-06 20:02:47 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,15 @@ void print_square(int size)
 {
         int i;
         int k;
-if (size == 0
+if (size == 0)
                 {
          _putchar('\n');
                 }
 else
 {
-for (i = 0; i <= size; i++)
+for (i = 0; i < size; i++)
 {
-        for (k = 0; k <= size; k++)
+        for (k = 0; k < size; k++)
         }
         _putchar(35);
 }
