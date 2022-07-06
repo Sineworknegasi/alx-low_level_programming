55c49cbd6b3580d05c4097983226f3172f567157
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2022-07-06 10:41:34 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,17 @@
-#include "main.h"
-/**
- * _isdigit - if is a digit
- *@c: print int
+#include<main.h>
+/*
+ * isdigit- if is a digit
+ * @c: print int
  * Return: Always 0.
  */
-
 int _isdigit(int c)
 {
-if (c >= 48 && c <= 57)
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
+        if (c >= 0 && c <= 9)
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
 }
